af186350191db281312788fbcb52b19e78f36fbb
Dingnh <18039503@imail.sunway.edu.my>
2020-08-10 02:24:32 +0800

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,46 +1,50 @@
 let data = {
-    media: [
-        {
-          link: "インスタ",
-          linkdir: "https://www.instagram.com/"
-        },
-        {
-          link: "ブーック",
-          linkdir: "https://www.facebook.com/"
-        },
-        {
-          link: "ユーチューブ",
-          linkdir: "https://www.youtube.com/"
-        },
-        {
-          link: "レジット",
-          linkdir: "https://www.reddit.com/"
-        }
-      ],
-      dev: [
-        {
-          link: "ジット",
-          linkdir: "https://www.github.com/"
-        },
-        {
-          link: "スレロ",
-          linkdir: "https://www.trello.com/"
-        }
-      ],
-      education: [
-        {
-          link: "メール",
-          linkdir: "http://www.imail.sunway.edu.my/"
-        },
-        {
-          link: "アイゾン",
-          linkdir: "https://izone.sunway.edu.my/"
-        },
-        {
-          link: "イーランー",
-          linkdir: "https://elearn.sunway.edu.my/"
-        }
-      ]
+  media: [
+    {
+      link: "インスタ",
+      linkdir: "https://www.instagram.com/"
+    },
+    {
+      link: "ブーック",
+      linkdir: "https://www.facebook.com/"
+    },
+    {
+      link: "ユーチューブ",
+      linkdir: "https://www.youtube.com/"
+    },
+    {
+      link: "レジット",
+      linkdir: "https://www.reddit.com/"
+    },
+    {
+      link: "アニメ",
+      linkdir: "https://www.gogoanime.movie/"
+    },
+  ],
+  dev: [
+    {
+      link: "ジット",
+      linkdir: "https://www.github.com/"
+    },
+    {
+      link: "スレロ",
+      linkdir: "https://www.trello.com/"
+    }
+  ],
+  education: [
+    {
+      link: "メール",
+      linkdir: "http://www.imail.sunway.edu.my/"
+    },
+    {
+      link: "アイゾン",
+      linkdir: "https://izone.sunway.edu.my/"
+    },
+    {
+      link: "イーランー",
+      linkdir: "https://elearn.sunway.edu.my/"
+    }
+  ]
 }
 
 export default data
